d376f4f62a23d659ee2512e83592463a0ee3307f
abindas1 <78995121+abindas1@users.noreply.github.com>
2021-02-23 09:11:32 +0530

    Add files via upload

---
+++ 4q2.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+yy = int(input(" Enter The Year :"))
+dd = int(input(" Enter The Date :"))
+mm = int(input(" Enter The Month :"))
+our_date = datetime(yy,mm,dd)
+print("The Date is :",our_date)
---
+++ 4q3.py
@@ -0,0 +1,13 @@
+# from time import time
+
+# time()
+
+
+import datetime
+#yy = int(input(" Enter The Year :"))
+#dd = int(input(" Enter The Date :"))
+#mm = int(input(" Enter The Month :"))
+our_date = datetime.datetime.now()
+print("The Date Time Is :",our_date)
+
+ 
